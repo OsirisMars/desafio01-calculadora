bbc91b5fba7b00869149ec3792f85bc1cf5d6de8
OsirisMars <83598355+OsirisMars@users.noreply.github.com>
2024-09-16 14:32:16 -0300

    Inicio do desafio 01 da DIO

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import { ButtonContainer } from "./styles";
+
+function Button() {
+    return (
+      <ButtonContainer>
+        Submit !
+      </ButtonContainer>
+    );
+  }
+  
+  export default Button;
+  
---
+++ src/components/Button/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+ 
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #C0C0C0;
+    background-color: #0000FF;
+`
